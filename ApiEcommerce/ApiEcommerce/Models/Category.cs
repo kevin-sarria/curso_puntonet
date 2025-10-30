e86994134918aabb7ec39823e386f564aed85006
Kevin Sarria <camilo.bravo.2050@gmail.com>
2025-10-30 02:19:11 -0500

    avances

---
+++ ApiEcommerce/ApiEcommerce/Models/Category.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public DateTime CreationDate { get; set; }
+}
---
+++ ApiEcommerce/ApiEcommerce/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var dbConnectionString = builder.Configuration.GetConnectionString("ConexionSql"); 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dbConnectionString));
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
